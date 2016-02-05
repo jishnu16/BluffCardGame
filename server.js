2b5109a8afa149b1a6062650e2d5851f16a03c35
Surajit Chongder <surajit.chongder@gmail.com>
2016-02-05 11:35:06 +0530

    Bluff table css changed

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ var Controller = require('./controller.js');
 var lib = require('./lib/games.js');
 var Games = lib.Games;
 var games = new Games();
-console.log(games);
 var controller = new Controller(games);
-http.createServer(controller).listen(4444);	
+http.createServer(controller).listen(4444);
